07492a85017b4a5a9d9f32436a5eb9d54d94047b
Elena <eamatytsina@gmail.com>
2022-05-15 23:41:20 +0300

    задание 4_2

--- src/view/sort-view.js
+++ src/view/sort-view.js
@@ -2,7 +2,8 @@ import AbstractView from '../framework/view/abstract-view.js';
 
 const createSortTemplate = () => (
   `<form class="trip-events__trip-sort  trip-sort" action="#" method="get">
-            <div class="trip-sort__item  trip-sort__item--day">
+
+          <div class="trip-sort__item  trip-sort__item--day">
               <input id="sort-day" class="trip-sort__input  visually-hidden" type="radio" name="trip-sort" value="sort-day" checked="">
               <label class="trip-sort__btn" for="sort-day">Day</label>
             </div>
